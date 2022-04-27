e009a2581180da3d19e5a96eb2d441799538f788
Felipe Maximiano <maximiano.felipesilva@gmail.com>
2022-04-27 18:53:17 -0300

    Conexão do Atlas com o Compass

--- src/database/db.js
+++ src/database/db.js
@@ -2,10 +2,13 @@ import mongoose from 'mongoose';
 
 export function connectToDatabase() {
   mongoose
-    .connect('mongodb://localhost:27017/lanches-db', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      'mongodb+srv://felipemaximiano:556758595a@cluster0.cetlg.mongodb.net/test',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    )
     .then(() => {
       console.log('MongoDB conectado');
     })
